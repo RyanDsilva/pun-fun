dbe593f2461bd75763ee3096c96aecd4f0da5dd1
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2022-11-10 06:14:48 -0500

    frontend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pprint
 from setup import setup
+import gradio as gr
 from target_identification.target_identification import run_inference_pipeline
 from sense_similarity.sense_similarity import find_sense_similarity, find_source_pos, find_synset
 from sound_similarity.sound_similarity import get_sound_similarity
@@ -7,9 +8,7 @@ from source_identification.inference import identify_source
 
 pp = pprint.PrettyPrinter(indent=2)
 
-if __name__ == "__main__":
-  setup()
-  sentence = "The boat store had a huge sail ."
+def run(sentence):
   source_word = identify_source(sentence)
   res = run_inference_pipeline(sentence, source_word)
   for r in res:
@@ -20,9 +19,35 @@ if __name__ == "__main__":
     src_sense = find_synset(src)
     pred_sense = find_synset(pred)
     if src_sense is not None and pred_sense is not None:
-      print(f"Sense similarity between {src} and {pred} is {find_sense_similarity(src_sense, pred_sense)}")
-    ss = get_sound_similarity(src, pred)
-    if ss is not None:
-      print(f"Sound similarity between {src} and {pred} is {get_sound_similarity(src, pred)}")
+      print("Definitions")
+      print(f"{src}: {src_sense.definition()}")
+      print(f"{pred}: {pred_sense.definition()}")
+      print("===========================================")
+      sense_sim = find_sense_similarity(src_sense, pred_sense)
+      print(f"Sense similarity between {src} and {pred} is {sense_sim}")
+    sound_sim = get_sound_similarity(src, pred)
+    if sound_sim is not None:
+      print(f"Sound similarity between {src} and {pred} is {sound_sim}")
     print("=============================================")
+    return source_word, src_sense.definition(), pred, pred_sense.definition(), sense_sim, sound_sim
+
+if __name__ == "__main__":
+  setup()
+  app = gr.Interface(
+    title='CNIT 519 - Project 2',
+    fn=run,
+    allow_flagging='never',
+    inputs=[
+      gr.Textbox(label='Input Sentence')
+    ],
+    outputs=[
+      gr.Textbox(label='Source'),
+      gr.Textbox(label='Source Definition'),
+      gr.Textbox(label='Target'),
+      gr.Textbox(label='Target Definition'),
+      gr.Textbox(label='Sense Similarity'),
+      gr.Textbox(label='Sound Similarity'),
+    ],
+  )
+  app.launch()
 
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import nltk
 
 def setup():
-  nltk.download('punkt')
-  nltk.download('stopwords')
-  nltk.download('wordnet')
-  nltk.download('omw-1.4')
-  nltk.download('cmudict')
-  nltk.download('averaged_perceptron_tagger')
+  nltk.download('punkt', quiet=True)
+  nltk.download('stopwords', quiet=True)
+  nltk.download('wordnet', quiet=True)
+  nltk.download('omw-1.4', quiet=True)
+  nltk.download('cmudict', quiet=True)
+  nltk.download('averaged_perceptron_tagger', quiet=True)
   print('[INFO] Setup completed successfully.')
